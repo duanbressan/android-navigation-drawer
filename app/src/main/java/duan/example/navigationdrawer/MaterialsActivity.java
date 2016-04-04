55c3bfee8d547a93180ae321f0d3caba096b43cf
duanbressan <duan.trabalho@hotmail.com>
2016-04-04 11:03:17 -0300

    Nova tela com TabBar Alterado estilo para NoActionBar Adicionado RecyclerView no Build.gradle

---
+++ app/src/main/java/duan/example/navigationdrawer/MaterialsActivity.java
@@ -0,0 +1,93 @@
+package duan.example.navigationdrawer;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import duan.example.navigationdrawer.Adapters.MyPagerAdapter;
+
+
+public class MaterialsActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private TabLayout mTabLayout;
+    private ViewPager mPager;
+    private MyPagerAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_materials);
+        mAdapter = new MyPagerAdapter(getSupportFragmentManager());
+        mToolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(mToolbar);
+        mTabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(mAdapter);
+        mTabLayout.setTabsFromPagerAdapter(mAdapter);
+
+        mTabLayout.setupWithViewPager(mPager);
+        mPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_materials, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public static class MyFragment extends Fragment {
+        public static final java.lang.String ARG_PAGE = "arg_page";
+
+        public MyFragment() {
+
+        }
+
+        public static MyFragment newInstance(int pageNumber) {
+            MyFragment myFragment = new MyFragment();
+            Bundle arguments = new Bundle();
+            arguments.putInt(ARG_PAGE, pageNumber + 1);
+            myFragment.setArguments(arguments);
+            return myFragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            Bundle arguments = getArguments();
+            int pageNumber = arguments.getInt(ARG_PAGE);
+            TextView myText = new TextView(getActivity());
+            myText.setText("Hello I am the text inside this Fragment " + pageNumber);
+            myText.setGravity(Gravity.CENTER);
+            return myText;
+        }
+    }
+}
+
